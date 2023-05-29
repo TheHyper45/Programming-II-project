216547b8c3a9094c3ef8bea83128a284d2f0a56e
Kacper <74537153+KasmyrA@users.noreply.github.com>
2023-05-29 21:15:54 +0200

    Applying class Game to menu

--- code/main.cpp
+++ code/main.cpp
@@ -7,36 +7,28 @@
 #include "platform.hpp"
 #include "renderer.hpp"
 #include "exceptions.hpp"
+#include "game.hpp"
 
 int main() {
     core::Platform platform = {};
     try {
         platform.create_main_window("Tanks",1024,768);
+        
         auto renderer = platform.create_renderer();
 
-        auto tiles_sprite_atlas = renderer.sprite_atlas("./assets/tiles_16x16.bmp",16);
-        std::uint32_t tile_index = 0;
-
+        core::Game game(&renderer, &platform);
+        game.menu();
         auto start_time = std::chrono::steady_clock::now();
         while(!platform.window_closed()) {
             auto end_time = std::chrono::steady_clock::now();
             float delta_time = float(std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count()) / 1000000.0f;
             start_time = end_time;
-
+            
             platform.process_events();
-            if(platform.was_key_pressed(core::Keycode::Space)) {
-                std::cout << "FPS: " << (1.0f / delta_time) << std::endl;
-            }
-            if(platform.was_key_pressed(core::Keycode::Right)) {
-                if(tile_index < 63) tile_index += 1;
-            }
-            if(platform.was_key_pressed(core::Keycode::Left)) {
-                if(tile_index > 0) tile_index -= 1;
-            }
+            game.logic();
 
             renderer.begin(delta_time);
-            renderer.draw_sprite({0.5f,0.5f},{1.0f,1.0f},0,tiles_sprite_atlas,4);
-            renderer.draw_sprite({core::Background_Tile_Count_X / 2.0f,core::Background_Tile_Count_Y / 2.0f},{8,8},0,tiles_sprite_atlas,tile_index);
+            game.graphics();
             renderer.end();
             platform.swap_window_buffers();
         }
