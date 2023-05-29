44e14ac96e1523ad38e83d761ebf6aa64fcd4898
Kacper <74537153+KasmyrA@users.noreply.github.com>
2023-05-29 20:40:47 +0200

    Update game.cpp

--- code/game.cpp
+++ code/game.cpp
@@ -27,7 +27,7 @@ namespace core {
 			renderer->draw_text({ 2.0f,5.0f,1 }, { 0.8f,1.0f }, { 1.0f,1.0f,(float)((menu_choice+2) % number_of_choices == 1 ? 0 : 1) }, "2 player");
 			renderer->draw_text({ 2.0f,6.0f,1 }, { 0.8f,1.0f }, { 1.0f,1.0f,(float)((menu_choice+2) % number_of_choices == 0 ? 0 : 1) }, "Construction");
 			break;
-		case Scene::Select_Lecel_1player:
+		case Scene::Select_Level_1player:
 			for (float i = 0; i < Background_Tile_Count_X; i += 1) {
 				for (float j = 0; j < Background_Tile_Count_Y; j += 1) {
 					renderer->draw_sprite({ 0.5f + i,0.5f + j }, { 1.0f,1.0f }, 0.0f, tiles_sprite_atlas, 5);
@@ -41,7 +41,7 @@ namespace core {
 		case Scene::Game_1player:
 		
 			break;
-		case Scene::Select_Lecel_2player:
+		case Scene::Select_Level_2player:
 			break;
 		case Scene::Game_2player:
 			break;
@@ -72,23 +72,23 @@ namespace core {
 			if (platform->was_key_pressed(core::Keycode::Return)) {
 				printf("Execute choice: %d\n", menu_choice);
 				if (menu_choice==3||menu_choice==0) {
-					scene = Scene::Select_Lecel_1player;
+					scene = Scene::Select_Level_1player;
 				}
 				if (menu_choice == 2) {
-					scene = Scene::Select_Lecel_2player;
+					scene = Scene::Select_Level_2player;
 				}
 				if (menu_choice == 1||menu_choice==4) {
 					scene = Scene::Construction;
 				}
 			}
 			break;
-		case Scene::Select_Lecel_1player:
+		case Scene::Select_Level_1player:
 			if (platform->was_key_pressed(core::Keycode::Return))
 				scene = Scene::Game_1player;
 			break;
 		case Scene::Game_1player:
 			break;
-		case Scene::Select_Lecel_2player:
+		case Scene::Select_Level_2player:
 			if (platform->was_key_pressed(core::Keycode::Return))
 				scene = Scene::Game_1player;
 			break;
