3645f5a067418e25695f9b952f90b4c46a68358d
Kacper <74537153+KasmyrA@users.noreply.github.com>
2023-06-13 22:30:21 +0200

    added explosions and enemies

--- code/game.hpp
+++ code/game.hpp
@@ -28,6 +28,7 @@ namespace core {
 		bool on_screen(Vec3 position, Vec2 size);
 		bool collision(Vec3 position1, Vec2 size1, Vec3 position2, Vec2 size2);
 		bool collision(Vec3 position1, Vec2 size1, Vec3 circle_position, float radius);
+		bool collision(Vec3 circle_position1, float radius1, Vec3 circle_position2, float radius2);
 		Game(core::Renderer* renderer,core::Platform* platform);
 		
 		class Tank {
@@ -36,6 +37,7 @@ namespace core {
 			Vec3 position = { 7.5f,10.5f,0 };
 			float rotation = 0;
 			Vec2 size = { 1.0f,1.0f };
+			Vec2 hitbox = { 0.9f,0.9f };
 			float direction = 0;
 			float distance_to_travel = 0;
 			void move(float direction);
@@ -43,6 +45,7 @@ namespace core {
 			void animate(float delta_time);
 			void go(float delta_time);
 			void shoot();
+			void destroyed();
 			
 
 			
@@ -50,6 +53,7 @@ namespace core {
 
 		
 		class Enemy_Tank {
+		public:
 			std::int32_t sprite_index = 14;
 			Vec3 position = { 0.5f,0.5f,0 };
 			float rotation = 0;
@@ -58,20 +62,35 @@ namespace core {
 			float distance_to_travel = 0;
 			int inteligence = 0;
 			int speed = 1;
-			int durability = 100;
+			int durability = 1;
 			int dmg = 100;
+			Vec2 hitbox = { 1.0f,1.0f };
 			float reloading_time = 10;
-		public:
+			
+	
 			Enemy_Tank(const std::int32_t& sprite_index, const Vec3& position, float rotation, const Vec2& size, float direction, float distance_to_travel, int inteligence, int speed, int durability, int dmg, float reloading_time)
 				: sprite_index(sprite_index), position(position), rotation(rotation), size(size), direction(direction), distance_to_travel(distance_to_travel), inteligence(inteligence), speed(speed), durability(durability), dmg(dmg), reloading_time(reloading_time)
-			{
-			}
+			{}
+			Enemy_Tank(const std::int32_t& sprite_index, const Vec3& position)
+				: sprite_index(sprite_index), position(position)
+			{}
+
+			bool operator == (const Enemy_Tank& b) const {
+				return position.x == b.position.x && position.y == b.position.y && position.z == b.position.z &&
+					size.x == b.size.x && size.y == b.size.y && rotation==b.rotation;
+			};
 		};
 		class Barrel {
 		public:
 			std::int32_t sprite_index=18;
 			Vec3 position;
 			Vec2 size;
+			float hitbox_radius = 0.5f;
+
+			bool operator == (const Barrel& b) const {
+				return position.x == b.position.x && position.y == b.position.y && position.z == b.position.z &&
+					size.x == b.size.x && size.y == b.size.y && hitbox_radius == b.hitbox_radius;
+			};
 		};
 
 	private:
@@ -79,6 +98,7 @@ namespace core {
 		Sprite_Index bullet_alias;
 		Sprite_Index player1_alias;
 		Sprite_Index eagle_alias;
+		Sprite_Index explosions_atlas;
 		Renderer* renderer;
 		Platform* platform;
 		Scene scene;
